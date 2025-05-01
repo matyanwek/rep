3a6328dcba71e9bf9c6fde2131831f199a536738
Gal Zeira <gal_zeira@protonmail.com>
2025-05-01 09:33:23 -0400

    fixed tests

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ tests=''
 tests="$tests test_file"
 test_file() {
 	echo 'hello world' > file_a.txt
-	local actual="$(rep file_a.txt)"
+	local actual="$(./rep file_a.txt)"
 	local expected="$(for i in $(seq 10); do echo 'hello world'; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
@@ -24,7 +24,7 @@ tests="$tests test_multiple_files"
 test_multiple_files() {
 	echo 'hello world' > file_a.txt
 	echo 'goodbye world' > file_b.txt
-	local actual="$(rep file_a.txt file_b.txt)"
+	local actual="$(./rep file_a.txt file_b.txt)"
 	local expected="$(for i in $(seq 10); do echo 'hello world'; echo 'goodbye world'; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
@@ -32,7 +32,7 @@ test_multiple_files() {
 
 tests="$tests test_stdin"
 test_stdin() {
-	local actual="$(echo 'hello world' | rep)"
+	local actual="$(echo 'hello world' | ./rep)"
 	local expected="$(for i in $(seq 10); do echo 'hello world'; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
@@ -42,7 +42,7 @@ tests="$tests test_stdin_with_files"
 test_stdin_with_files() {
 	echo 'hello world' > file_a.txt
 	echo 'goodbye world' > file_b.txt
-	local actual="$(echo 'good day world' | rep file_a.txt - file_b.txt)"
+	local actual="$(echo 'good day world' | ./rep file_a.txt - file_b.txt)"
 	local expected="$(for i in $(seq 10); do echo 'hello world'; echo 'good day world'; echo 'goodbye world'; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
@@ -50,7 +50,7 @@ test_stdin_with_files() {
 
 tests="$tests test_large_stdin"
 test_large_stdin() {
-	local actual="$(yes 'hello world' | head -n 1000 | rep)"
+	local actual="$(yes 'hello world' | head -n 1000 | ./rep)"
 	local expected="$(for i in $(seq 10); do yes 'hello world' | head -n 1000; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
@@ -59,7 +59,7 @@ test_large_stdin() {
 tests="$tests test_nflag"
 test_nflag() {
 	echo 'hello world' > file_a.txt
-	local actual="$(rep -n 20 file_a.txt)"
+	local actual="$(./rep -n 20 file_a.txt)"
 	local expected="$(for i in $(seq 20); do echo 'hello world'; done)"
 	[ "x$actual" = "x$expected" ]
 	return $?
